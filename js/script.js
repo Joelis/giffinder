ac166c84957e9acf545892ad91f7e0bd63008808
Joelis Paula <jpaula@cristoreyny.org>
2018-04-27 12:55:20 +0000

    dog gif

--- js/script.js
+++ js/script.js
@@ -4,7 +4,20 @@
 
 $(document).ready(function(){
   
-  
+  $.ajax({
+  url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
+  method: "GET",
+  success: function(response) {
+       
+        console.log(response.data[0].images.fixed_width.url);
+        
+        var gif = response.data[0].images.fixed_width.url;
+        https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC
+        
+        $('body').append('<img src=' + gif + '>');
+        
+  },
+});
   
   
 });
